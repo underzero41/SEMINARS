4e1c4ee802b3a04eb07f251f840c48c969f154f3
1chello <underzero@bk.ru>
2023-01-14 17:55:52 +0300

    Task 3 done

---
+++ Task3/Program.cs
@@ -0,0 +1,18 @@
+﻿//Напишите программу котораю будет на ввод принимать число, а на вывод выдавать день недели.
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+if(number==1)
+Console.Write("Понедельник");
+if(number==2)
+Console.Write("Вторник");
+if(number==3)
+Console.Write("Среда");
+if(number==4)
+Console.Write("Четверг");
+if(number==5)
+Console.Write("Пятница");
+if(number==6)
+Console.Write("Суббота");
+if(number==7)
+Console.Write("Воскресенье");
