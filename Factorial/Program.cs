670f2fbcc588644daa45d431f9427601456144f8
1chello <underzero@bk.ru>
2023-01-15 17:41:20 +0300

    factorial

---
+++ Factorial/Program.cs
@@ -0,0 +1,13 @@
+﻿//Нахождение факториала
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+int sum= 0;
+int count= 1;
+
+while (sum<number){
+    sum+=1;
+    count=sum*count;
+    
+}
+Console.Write(count);
