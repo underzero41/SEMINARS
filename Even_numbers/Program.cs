4bb6b15e5c27d48fc2df74deb2665ef4bd20d7b4
1chello <underzero@bk.ru>
2023-01-15 18:03:14 +0300

    not finished even numbers

---
+++ Even_numbers/Program.cs
@@ -0,0 +1,15 @@
+﻿// Нахождение всех четных чисел до определенного
+Console.Write("Напишите число: ");
+int number = int.Parse(Console.ReadLine());
+int sum = 0;
+int count = 1;
+while (count <= number){
+    if (count%2==0){
+        sum+=count;
+        count+=count;
+    }
+    else{
+        count+=count;
+    }
+}
+Console.Write(sum);
