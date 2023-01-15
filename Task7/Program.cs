1da36726fdf1a39288ae1a8b5b903b8ec01e5f17
1chello <underzero@bk.ru>
2023-01-15 07:59:04 +0300

    Add Task7

--- Task7/Program.cs
+++ Task7/Program.cs
@@ -1,1 +1,7 @@
 ﻿//Напишите программу кторая на вход принимает трехзначное число, а на выходе показывает последнюю цифру этого числа.
+
+Console.Write("Input three digit number: ");
+int number = int.Parse(Console.ReadLine());
+Console.Write(number + " -> ");
+
+Console.Write(number%10 + " ");
