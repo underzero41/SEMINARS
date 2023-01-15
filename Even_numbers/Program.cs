99d544f1d01daa57253c67f56cda8c1bcdd87b88
1chello <underzero@bk.ru>
2023-01-15 18:52:14 +0300

    Task 2

--- Even_numbers/Program.cs
+++ Even_numbers/Program.cs
@@ -1,15 +1,16 @@
 ﻿// Нахождение всех четных чисел до определенного
 Console.Write("Напишите число: ");
 int number = int.Parse(Console.ReadLine());
-int sum = 0;
-int count = 1;
+int sum = 0; //1
+int count = 1; //2
 while (count <= number){
     if (count%2==0){
         sum+=count;
-        count+=count;
+        count+=count;   
     }
-    else{
+    else {
         count+=count;
-    }
+    } 
+    Console.Write($"{sum}, ");
 }
-Console.Write(sum);
+//незаконченное 
