a74c775bd9426a7a36900962504054f60ef29f44
1chello <underzero@bk.ru>
2023-01-16 08:26:58 +0300

    Task 8 done

---
+++ Task8/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+Console.WriteLine("Input number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int current = 0;
+while (current<=number){
+    if (current%2==0){
+        Console.Write($"{current}, ");
+    }
+    current+=1;
+}
+Console.Write("\b\b are even numbers.");    
