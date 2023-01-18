e517239b5646d9922c9cd5cdc01fa260f1dc3319
1chello <underzero@bk.ru>
2023-01-18 16:08:50 +0300

    Add seminars task 1 and 2

---
+++ Seminar_2.1/Program.cs
@@ -0,0 +1,6 @@
+﻿// Напишите программу, которая выводит случайное трехзначное число и удаляет вторую цифру из этого числа.
+int num = new Random().Next(100, 1000);
+Console.WriteLine(num);
+int num1 = num/100;
+int num2 = num%10;
+Console.WriteLine($"{num1}{num2}");
---
+++ Seminar_2/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая выводит случайное число из отрезка [10,99] и 
+// показывает наибольшую цифру числа.
+
+int num = new Random().Next(10,100);
+Console.WriteLine(num);
+int num2 = num%10;
+int num1 = num/10;
+//Console.WriteLine(num1);
+//Console.WriteLine(num2);
+
+if(num1>num2){
+    Console.WriteLine("Наибольшая цифра числа {0} равна {1}",num, num1);
+}
+else{
+    //интерполяция
+    Console.WriteLine("Наибольшая цифра числа {0} равна {1}",num, num2);
+    //Console.WriteLine($"Наибольшая цифра числа {num} равна {num2}");
+    //конкатенация
+    //Console.WriteLine("Наибольшая цифра числа " + num + " равна " + num2);
+
+}
