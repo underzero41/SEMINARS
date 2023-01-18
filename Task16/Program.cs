c4769079b8e5b2f7926a5b6d39179c7ff90464e1
1chello <underzero@bk.ru>
2023-01-18 17:03:52 +0300

    task 16 done

---
+++ Task16/Program.cs
@@ -0,0 +1,12 @@
+﻿//Напишите программу, которая принимает на вход два числа и проверяет, является ли
+//одно число квадратом другого.
+Console.Write("Input first number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input second number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+if (num1*num1==num2){
+    Console.WriteLine($"Number {num1} is a square root of {num2}, cool, you are the best dev!");
+}
+else{
+    Console.WriteLine($"Number {num1} is not a square root of {num2}, sh*t! But you are cool, however");
+}
