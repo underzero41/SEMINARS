8db86e358cd2752e0909bb988f4224907d45040a
1chello <underzero@bk.ru>
2023-01-18 17:08:42 +0300

    task 16 change a litt

--- Task16/Program.cs
+++ Task16/Program.cs
@@ -5,7 +5,10 @@ int num1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Input second number: ");
 int num2 = Convert.ToInt32(Console.ReadLine());
 if (num1*num1==num2){
-    Console.WriteLine($"Number {num1} is a square root of {num2}, cool, you are the best dev!");
+    Console.WriteLine($"Number {num2} is a square root of {num1}, cool, you are the best dev!");
+}
+else if (num2*num2==num1){
+    Console.WriteLine($"Number {num1} is a square root of {num2}, cool, you are the best dev!");    
 }
 else{
     Console.WriteLine($"Number {num1} is not a square root of {num2}, sh*t! But you are cool, however");
