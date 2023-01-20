fba52e9850b437dea49d44df8ec99e6d0263b7f3
1chello <underzero@bk.ru>
2023-01-20 07:03:39 +0300

    fix task15

--- Task15/Program.cs
+++ Task15/Program.cs
@@ -5,10 +5,13 @@
 
 Console.Write("Write the number for the day of the week: ");
 int num = Convert.ToInt32(Console.ReadLine());
-if (num == 6 || num == 7) {
-  Console.WriteLine($"{num} -> yes, today is a day off, take a break, relax");
+void checkTheDayOfTheWeek (int num){
+  if (num == 6 || num == 7) {
+    Console.WriteLine($"{num} -> yes, today is a day off, take a break, relax");
   }
   else if (num < 1 || num > 7) {
     Console.WriteLine($"{num} -> It's not the day of the week, input range is 1 to 7, try one more time.");
   }
   else Console.WriteLine($"{num} -> Nope, today is a weekday, work hard, play hard.");
+}
+checkTheDayOfTheWeek(num);
