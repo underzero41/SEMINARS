64d4391133bc037fff38a0c007ffd8de398a1024
1chello <underzero@bk.ru>
2023-01-25 16:33:54 +0300

    add Task26

--- Task26/Program.cs
+++ Task26/Program.cs
@@ -2,3 +2,13 @@
 
 Console.Write("Input number: ");
 int number = Convert.ToInt32(Console.ReadLine());
+int nums = 0;
+int count = number;
+
+while(count > 0)
+{
+    count /= 10;
+    nums++;
+}
+
+Console.WriteLine($"In number {number} -> {nums} numbers");
