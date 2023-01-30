74c4dc821de73e6ece11148feb278bff431b2dd4
1chello <underzero@bk.ru>
2023-01-30 12:51:59 +0300

    add task35

--- Task35/Program.cs
+++ Task35/Program.cs
@@ -4,3 +4,29 @@
 // [5, 18, 123, 6, 2] -> 1
 // [1, 2, 3, 6, 2] -> 0
 // [10, 11, 12, 13, 14] -> 5
+
+int[] arr = new int[123];
+FillArray(arr);
+Console.Write(String.Join(", ", arr));
+Console.Write(" -> ");
+Console.WriteLine(CheckNumbers(arr));
+
+
+
+void FillArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(1,100);
+    }
+}
+
+int CheckNumbers(int[] arr)
+{
+    int counter = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+       if(arr[i] >= 10 && arr[i] <= 99) counter++;
+    }
+    return counter;
+}
