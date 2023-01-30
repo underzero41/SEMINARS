f727e6a097c303ef6dcbbc0c2447b04545d08013
1chello <underzero@bk.ru>
2023-01-30 11:49:58 +0300

    add task32 and 33

---
+++ Task32/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 32: Напишите программу замена элементов массива: 
+// положительные элементы замените на соответствующие отрицательные, и наоборот.
+// [-4, -8, 8, 2] -> [4, 8, -8, -2]
+
+int[] arr = new int[4];
+
+Console.Write('[');
+for(int i = 0; i < arr.Length; i++)
+{
+    arr[i] = new Random().Next(-4,11);
+    Console.Write(arr[i] + " ");
+}
+Console.Write(']');
+
+Console.Write('[');
+for(int i = 0; i < arr.Length; i++)
+{
+    arr[i] *= -1; 
+    Console.Write(arr[i] + " ");
+}
+Console.Write(']');
---
+++ Task33/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 33: Задайте массив. Напишите программу, которая определяет, 
+// присутствует ли заданное число в массиве.
+// 4; массив [6, 7, 19, 345, 3] -> нет
+// -3; массив [6, 7, 19, 345, 3] -> да
+
+int[] arr = new int[5];
+void FillandPrintArray (int[] arr)
+{
+    Console.Write('[');
+    for(int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(-9,10);
+        Console.Write(arr[i] + " ");
+    }
+    Console.WriteLine(']');
+}
+FillandPrintArray(arr);
+
+
+Console.Write("Input number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if(arr.Contains(number) || arr.Contains(number * -1)) Console.WriteLine("Yes");
+else Console.WriteLine("No");
+
+// for (int i = 0; i < arr.Length; i++)
+// {
+//     if(number == arr[i] || number * -1 == arr[i]) 
+//     {
+//         Console.WriteLine("Yes");
+//         return;
+//     }
+//     else Console.WriteLine("No");
+// }
