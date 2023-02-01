844074594c07e5693a6ac7a433a867c85832e762
1chello <underzero@bk.ru>
2023-02-01 16:38:36 +0300

    Add Task40

---
+++ Task40/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая на вход принимает три числа и проверяет,
+// может ли существовать треугольник со сторонами такой длины.
+
+Console.Write("Input size of side A: ");
+int sideA = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input size of side B: ");
+int sideB = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input size of side C: ");
+int sideC = Convert.ToInt32(Console.ReadLine());
+
+void CheckTringle(int sideA, int sideB, int sideC)
+{
+    if (sideA < sideB + sideC && sideB < sideA + sideC && sideC < sideA + sideB)
+    {
+        Console.WriteLine("Tringle ABC is correct");
+    }
+    else Console.WriteLine("Tringle ABC doesn't exist.");
+}
+
+CheckTringle(sideA, sideB, sideC);
+
+
+// Console.WriteLine($"Side A is {sideA}; Side B is {sideB}; Side C is {sideC}");
