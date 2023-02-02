c3df54e043d5d41060f250b0af67f7bd0dc2db37
1chello <underzero@bk.ru>
2023-02-02 08:21:45 +0300

    Add Task42

---
+++ Task42(двоичная система)/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу которая будет преобразовывать десятичное число в двоичное.
+
+Console.Write("Input number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+string Perevod(int temp)
+{
+    string bin = "";
+    // int i=0;
+    int temp1 = 0;
+    while(temp > 0)
+    {
+      temp1 = temp % 2;
+      temp = temp / 2;
+      bin = bin + temp1;
+      // Console.Write(temp1);
+
+    }
+    return bin;
+} 
+
+
+string bin = Perevod(num);
+
+// tmp = 0;
+for (int i = bin.Length - 1; i >= 0; i--)
+{
+  Console.Write($"{bin[i]} ");
+}
