afd52cc572c8895b4e624ed71316142741703071
1chello <underzero@bk.ru>
2023-02-03 20:46:43 +0300

    Add Task46,48 done

---
+++ Task46/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 46: Задайте двумерный массив размером m×n, заполненный случайными целыми числами.
+// m = 3, n = 4.
+// 1 4 8 19
+// 5 -2 33 -2
+// 77 3 8 1
+
+Console.Write("Input size of array m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input size of array n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[,] array = new int[m,n];
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i,j] = new Random().Next(-9, 10);
+    }
+}
+
+PrintArray(array);
+
+void PrintArray(int[,] arr)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        Console.Write($"{arr[i,j]}\t");
+    }
+    Console.WriteLine();
+}
+}
---
+++ Task48/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 48: Задайте двумерный массив размера m на n,
+// каждый элемент в массиве находится по формуле: Aₘₙ = m+n. Выведите полученный массив на экран.
+// m = 3, n = 4.
+// 0 1 2 3
+// 1 2 3 4
+// 2 3 4 5
+
+Console.Write("Input size of array m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input size of array n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[,] array = new int[m,n];
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i,j] = i + j;
+    }
+}
+
+void PrintArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+{
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+        Console.Write($"{array[i,j],4}");
+    }
+    Console.WriteLine();
+}
+}
+
+PrintArray(array);
