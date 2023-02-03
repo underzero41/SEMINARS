acbdaa16f3e1320de17581f345fa3de0fc5400f6
1chello <underzero@bk.ru>
2023-02-03 21:27:40 +0300

    Add Task49,51 done

---
+++ Task49/Program.cs
@@ -0,0 +1,63 @@
+﻿// Задача 49: Задайте двумерный массив. Найдите элементы, 
+// у которых оба индекса не чётные, и замените эти элементы на их квадраты.
+// Например, изначально массив
+//  выглядел вот так:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Новый массив будет выглядеть 
+// вот так:
+// 1 4 7 2
+// 5 81 2 9
+// 8 4 2 4
+
+int[,] array = new int[5, 5];
+array = FillArray(array);
+
+
+int[,] FillArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = new Random().Next(0, 11);
+        }
+    }
+    return arr;
+}
+
+
+
+void PrintArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i,j],4}");
+        }
+        Console.WriteLine();
+    }
+}
+
+PrintArray(array);
+
+void PowArray(int [,] arr)
+{
+    Console.WriteLine();
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+           if(i%2!=0 && j%2!=0)
+           {
+            arr[i,j] = Convert.ToInt32(Math.Pow(arr[i,j],2));
+           }
+        }
+    }
+    PrintArray(arr);
+
+}
+
+PowArray(array);
---
+++ Task51/Program.cs
@@ -0,0 +1,63 @@
+﻿// Задача 51: Задайте двумерный массив. Найдите сумму элементов, 
+// находящихся на главной диагонали (с индексами (0,0); (1;1) и т.д.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Сумма элементов главной диагонали: 1+9+2 = 12
+
+int[,] array = new int[5, 5];
+array = FillArray(array);
+
+
+int[,] FillArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = new Random().Next(0, 11);
+        }
+    }
+    return arr;
+}
+
+
+
+void PrintArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i,j],4}");
+        }
+        Console.WriteLine();
+    }
+}
+
+PrintArray(array);
+
+void DiogonalSumm(int [,] arr)
+{
+    int sum = 0;
+    Console.WriteLine("Summ of diogonal number: ");
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+           if(i==j)
+           {
+            Console.Write($"{arr[i,j]} + ");
+            sum +=  arr[i,j];
+           }
+        }
+     }
+     Console.Write("\b\b= ");
+     Console.Write($"{sum}");
+}
+    
+
+
+
+DiogonalSumm(array);
