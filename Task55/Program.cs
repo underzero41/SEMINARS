c6c68e086de6ab9811a14c613af61ece703d2da1
1chello <underzero@bk.ru>
2023-02-10 07:15:10 +0300

    Add Task 55

--- Task55/Program.cs
+++ Task55/Program.cs
@@ -1,2 +1,48 @@
 ﻿// Задайте двумерный массив. Напишите программу, которая заменяет строки на столбцы.
 // В случае, если это невозможно, программа должна вывести сообщение для пользователя.
+
+int[,] array = new int[5, 5];
+array = FillArray(array);
+
+int[,] FillArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = new Random().Next(0, 11);
+        }
+    }
+    return arr;
+}
+
+void PrintArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i, j],4}");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] ChangeArray(int[,] arr)
+{
+    int[,] arr2 = new int[arr.GetLength(1), arr.GetLength(0)];
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr2[j,i] = arr[i,j];
+        }
+    }
+    return arr2;
+}
+
+FillArray(array);
+PrintArray(array);
+int[,] arr2 = ChangeArray(array);
+Console.WriteLine();
+PrintArray(arr2);
