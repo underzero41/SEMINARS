2e678b2c5b2adcc0917e669df50b40bbaab09645
1chello <underzero@bk.ru>
2023-02-11 17:28:59 +0300

    Add Task69

--- Task69/Program.cs
+++ Task69/Program.cs
@@ -2,3 +2,23 @@
 // и возводит число А в целую степень B с помощью рекурсии.
 // A = 3; B = 5 -> 243 (3⁵)
 // A = 2; B = 3 -> 8
+
+int num = InputNumber("Input number: ");
+int pow = InputNumber("Input pow: ");
+int result = PowNum(num, pow);
+
+
+int InputNumber(string input)
+{
+    Console.Write(input);
+    int output = Convert.ToInt32(Console.ReadLine());
+    return output;
+}
+
+int PowNum(int num, int pow)
+{
+    if(pow == 1) return num;
+    else return num * PowNum(num, pow-1);
+}
+
+Console.WriteLine($"Num = {num}; Pow = {pow} -> {result}");
